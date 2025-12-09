68c1485e863fd55dc7aa6b384aefca80d33c2f55
Hemant-Singh0 <gouravs2250@gmail.com>
2025-12-09 23:22:02 +0530

    Add grid layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,18 @@
+import "./Home.css";
+
 const Home = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <div className="home-container">
+      <div className="card-grid">
+        <div className="card">Card 1</div>
+        <div className="card">Card 2</div>
+        <div className="card">Card 3</div>
+        <div className="card">Card 4</div>
+        <div className="card">Card 5</div>
+        <div className="card">Card 6</div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
